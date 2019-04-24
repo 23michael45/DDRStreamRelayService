e587c759d0f789291d9ad6a48eef4a889eb32323
23michael45 <michael2345@live.cn>
2019-04-24 11:15:37 +0800

    Base Include Dir to Shared and ProjectDir

--- DDRStreamRelayService/Version.h
+++ DDRStreamRelayService/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <string>
-std::string g_BuildTime = "3/29/2019 9:50:14 AM";
+std::string g_BuildTime = "4/24/2019 10:58:53 AM";
 std::string g_Version = "1.0.0";
 
 #ifdef _DEBUG
